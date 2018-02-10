e70b277f950871afdd8354f6681508254bd13aac
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-02-10 15:49:13 -0800

    autofac

---
+++ src/AlmVR.Server/AlmVR.Server.Core/ServiceProviderFactory.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlmVR.Server.Core
+{
+    public class ServiceProviderFactory
+    {
+        private static bool _containerCreated;
+
+        public static IServiceProvider GetServiceProvider(IServiceCollection services)
+        {
+            if (_containerCreated)
+                throw new NotSupportedException("GetServiceProvider can only be called once.");
+
+            var builder = new ContainerBuilder();
+            //builder.RegisterType<PostRepository>().As<IPostRepository>();
+            //builder.RegisterType<SiteAnalyticsServices>();
+            builder.Populate(services);
+            var container = builder.Build();
+            //Create the IServiceProvider based on the container.
+
+            _containerCreated = true;
+
+            return new AutofacServiceProvider(container);
+        }
+    }
+}
--- src/AlmVR.Server/AlmVR.Server/Startup.cs
+++ src/AlmVR.Server/AlmVR.Server/Startup.cs
@@ -26,7 +26,7 @@ namespace AlmVR.Server
         {
             services.AddMvc();
 
-            return new MefServiceProvider();
+            return ServiceProviderFactory.GetServiceProvider(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
