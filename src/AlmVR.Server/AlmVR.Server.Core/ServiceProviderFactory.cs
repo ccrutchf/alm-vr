fb8e1e9e06dbf8168bc7f2b21d798c2f773df936
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-02-10 16:06:14 -0800

    we have cookies (plugins)

--- src/AlmVR.Server/AlmVR.Server.Core/ServiceProviderFactory.cs
+++ src/AlmVR.Server/AlmVR.Server.Core/ServiceProviderFactory.cs
@@ -3,6 +3,10 @@ using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using System.Text;
 
 namespace AlmVR.Server.Core
@@ -16,16 +20,34 @@ namespace AlmVR.Server.Core
             if (_containerCreated)
                 throw new NotSupportedException("GetServiceProvider can only be called once.");
 
-            var builder = new ContainerBuilder();
-            //builder.RegisterType<PostRepository>().As<IPostRepository>();
-            //builder.RegisterType<SiteAnalyticsServices>();
-            builder.Populate(services);
-            var container = builder.Build();
-            //Create the IServiceProvider based on the container.
-
             _containerCreated = true;
 
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            LoadPlugins(builder);
+
+            var container = builder.Build();
+            //Create the IServiceProvider based on the container.
             return new AutofacServiceProvider(container);
+        }
+
+        private static void LoadPlugins(ContainerBuilder builder)
+        {
+            var exeLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var pluginLocaiton = Path.Combine(exeLocation, "Plugins");
+
+            var plugins = Directory.GetFiles(pluginLocaiton, "*.dll", SearchOption.AllDirectories)
+              .Select(path => AssemblyLoadContext.Default.LoadFromAssemblyPath(path))
+              .Where(x => x != null)
+              .Select(x => x.GetTypes())
+              .SelectMany(x => x)
+              .Where(x => x.GetInterfaces().Contains(typeof(IPlugin)))
+              .Select(x => Activator.CreateInstance(x) as IPlugin);
+
+            foreach (var plugin in plugins)
+            {
+                plugin.Initialize(builder);
+            }
         }
     }
 }
---
+++ src/AlmVR.Server/AlmVR.Server.Providers.Trello/TrelloPlugin.cs
@@ -0,0 +1,14 @@
+﻿using AlmVR.Server.Core;
+using Autofac;
+using System;
+
+namespace AlmVR.Server.Providers.Trello
+{
+    public class TrelloPlugin : IPlugin
+    {
+        public void Initialize(ContainerBuilder bulider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
