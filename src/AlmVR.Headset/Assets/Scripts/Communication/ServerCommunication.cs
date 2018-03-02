8ff1393d7e0e2806a7557c62bd25d9ed3174e79d
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-03-02 13:36:18 -0800

    unity UI for testing (doesn't work)

---
+++ src/AlmVR.Headset/Assets/Scripts/Communication/ServerCommunication.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using AlmVR.Client;
+using AlmVR.Client.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerCommunication : MonoBehaviour {
+
+    public GameObject StdOut;
+    public GameObject PortInputField;
+
+    private Text stdoutText;
+    private InputField portInputFieldText;
+    private StringBuilder stdoutBuilder = new StringBuilder();
+
+	// Use this for initialization
+	void Start () {
+        stdoutText = StdOut.GetComponent<Text>();
+        portInputFieldText = PortInputField.GetComponent<InputField>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Connect()
+    {
+        AppendLine("Connecting...");
+        string ipaddress = "localhost";
+
+        string portString = portInputFieldText.text;
+        int port = int.Parse(portString);
+
+        AppendLine($"Connecting to ws://{ipaddress}:{port}...");
+
+        var boardClient = ClientFactory.GetInstance<IBoardClient>();
+        boardClient.ConnectAsync(ipaddress, port).Wait();
+        AppendLine(boardClient.DoThingToServerAsync().Result);
+
+        boardClient.ThingHappenedToMe += BoardClient_ThingHappenedToMe;
+    }
+
+    private void AppendLine(string line)
+    {
+        stdoutBuilder.AppendLine(line);
+
+        stdoutText.text = stdoutBuilder.ToString();
+    }
+
+    private void BoardClient_ThingHappenedToMe(object sender, EventArgs e)
+    {
+        AppendLine("stop");
+    }
+}
