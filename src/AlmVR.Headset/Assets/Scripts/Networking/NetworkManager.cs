a441b9be80aa91960cc68639e146262b259cfada
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-04-16 22:06:53 -0700

    first stab at networking

--- src/AlmVR.Headset/Assets/Scripts/Networking/NetworkManager.cs
+++ src/AlmVR.Headset/Assets/Scripts/Networking/NetworkManager.cs
@@ -1,16 +1,75 @@
-﻿using System.Collections;
+﻿using Photon;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworkManager : MonoBehaviour {
+public class NetworkManager : PunBehaviour {
 
-	// Use this for initialization
+    public enum EventCode : byte
+    {
+        InstantiateVRAvatar
+    }
+
+    public string RoomName;
+    public GameObject LocalAvatar;
+
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        PhotonNetwork.ConnectUsingSettings("v4.2");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = false;
+        roomOptions.MaxPlayers = 8;
+        PhotonNetwork.JoinOrCreateRoom(RoomName, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        int viewId = PhotonNetwork.AllocateViewID();
+
+        PhotonNetwork.RaiseEvent((int)EventCode.InstantiateVRAvatar, viewId, true, new RaiseEventOptions() { CachingOption = EventCaching.AddToRoomCache, Receivers = ReceiverGroup.All });
+    }
+
+    public void OnEnable()
+    {
+        PhotonNetwork.OnEventCall += OnEvent;
+    }
+
+    public void OnDisable()
+    {
+        PhotonNetwork.OnEventCall -= OnEvent;
+    }
+
+    private void OnEvent(byte eventcode, object content, int senderid)
+    {
+        EventCode @event = (EventCode)eventcode;
+
+        if (@event == EventCode.InstantiateVRAvatar)
+        {
+            GameObject go = null;
+
+            if (PhotonNetwork.player.ID == senderid)
+            {
+                go = LocalAvatar;
+            }
+            else
+            {
+                go = Instantiate(Resources.Load("RemoteAvatar")) as GameObject;
+            }
+
+            if (go != null)
+            {
+                PhotonView pView = go.GetComponent<PhotonView>();
+
+                if (pView != null)
+                {
+                    pView.viewID = (int)content;
+                }
+            }
+        }
+    }
 }
