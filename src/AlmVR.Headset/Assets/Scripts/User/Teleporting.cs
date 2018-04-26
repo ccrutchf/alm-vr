f2affd1c6606cba70a26dab7d5083845164382c7
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-04-26 13:26:34 -0700

    teleportation, kinda

---
+++ src/AlmVR.Headset/Assets/Scripts/User/Teleporting.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour {
+
+    private bool wasStickHeldLastFrame;
+    private Vector3 currentTargetPosition;
+
+    public GameObject Island;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OVRInput.Update();
+
+        var primaryThumbstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        var lineRender = GetComponent<LineRenderer>();
+        if (primaryThumbstick.magnitude > 0.01f)
+        {
+            lineRender.enabled = true;
+
+            var rTouchLocalPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            var rTouchWorldPosition = transform.TransformPoint(rTouchLocalPosition);
+
+            var rTouchLocalForward = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch) * Vector3.forward;
+            var rTouchWorldForward = transform.TransformDirection(rTouchLocalForward);
+
+            var ray = new Ray(rTouchWorldPosition, rTouchWorldForward);
+            float distance = 0;
+            if (Island.GetComponent<Collider>().bounds.IntersectRay(ray, out distance))
+            {
+                currentTargetPosition = ray.GetPoint(distance);
+
+                lineRender.SetPositions(new Vector3[]
+                {
+                   rTouchWorldPosition,
+                   currentTargetPosition
+                });
+            }
+
+            wasStickHeldLastFrame = true;
+        }
+        else
+        {
+            lineRender.enabled = false;
+
+            if (wasStickHeldLastFrame)
+            {
+                transform.position = currentTargetPosition + new Vector3(0, 1.5f, 0);
+            }
+
+            wasStickHeldLastFrame = false;
+        }
+	}
+}
