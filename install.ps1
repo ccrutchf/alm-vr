363ca2ebaaa57d039b1659d0babcd82a781c7b07
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-05-17 11:46:18 -0700

    no progress

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 # Force Unity version.
-& choco install unity --version 2018.1.0 -y
+& choco install unity --version 2018.1.0 -y --no-progress
 
 [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files\Unity\Editor\Unity.exe")
