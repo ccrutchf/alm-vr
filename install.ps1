4d40cfe4d98a75aa0b1b53bbfedfee222be7cafb
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-05-22 10:49:27 -0700

    install prerelease software

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # Add MyGet repo
 choco source add -n="Alm-VR" -s="https://www.myget.org/F/alm-vr/api/v2/package"
 
-& choco install dotnetcore-sdk --source Alm-VR --version 2.1.300-rc1 -y --no-progress
+& choco install dotnetcore-sdk --source Alm-VR --version 2.1.300-rc1 -y --no-progress --pre
 
 # Force Unity version.
 & choco install unity --version 2018.1.0 -y --no-progress
